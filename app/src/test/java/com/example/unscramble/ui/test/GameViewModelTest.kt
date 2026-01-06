afa92a47871c3b29f733b241b08a6f1335944821
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2026-01-06 21:38:14 +0530

    Implemented the Error Path Tests and Improved the Test logic with proper placement of variables according to state

--- app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -1,33 +1,51 @@
 package com.example.unscramble.ui.test
-
 import com.example.unscramble.data.SCORE_INCREASE
 import com.example.unscramble.data.getUnscrambledWord
-import com.example.unscramble.ui.GameUiState
 import com.example.unscramble.ui.GameViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
-import kotlinx.coroutines.flow.StateFlow
+import junit.framework.TestCase.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class GameViewModelTest {
-    private val viewModel = GameViewModel()
+    private lateinit var viewModel: GameViewModel
 
-    @Test
-    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset()  {
-        var uiState: StateFlow<GameUiState> = viewModel.uiState
-        val currentGameUiState = uiState
-        val correctPlayerWord = getUnscrambledWord(currentGameUiState.value.currentScrambledWord)
-
-        viewModel.updateUserGuess(correctPlayerWord)
-        viewModel.checkUserGuess()
-
-        assertFalse(currentGameUiState.value.isGuessedWordWrong)
-        assertEquals(SCORE_AFTER_CORRECT_ANSWER, currentGameUiState.value.score)
+    @Before
+    fun setup(){
+        viewModel = GameViewModel()
     }
 
     //It is similar to using static in java
     companion object {
         private const val SCORE_AFTER_CORRECT_ANSWER  = SCORE_INCREASE
     }
+
+    //success path test
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset()  {
+        val currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        val updatedState = viewModel.uiState.value
+        assertFalse(updatedState.isGuessedWordWrong)
+        assertEquals(SCORE_AFTER_CORRECT_ANSWER, updatedState.score)
+    }
+
+    //error path test
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet(){
+        val inCorrectPlayerWord = "and"
+        viewModel.updateUserGuess(inCorrectPlayerWord)
+        viewModel.checkUserGuess()
+
+        val currentGameUiState = viewModel.uiState.value
+        assertEquals(0, currentGameUiState.score)
+        assertTrue(currentGameUiState.isGuessedWordWrong)
+    }
+
 
 }
